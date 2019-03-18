5edec1b49f83d72ca8d79a6968f78b57925c23d5
olliepalmer <github@olliepalmer.com>
2019-03-18 18:34:02 +0100

    Changed shutdown script; made text selection impossible

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/shutdown', function(req, res) {
-    res.send('ok, goodbye');
+    // res.send('ok');
     console.log('shutting down!');
     res.redirect('/');
     exec('sudo shutdown -h now', (err, stdout, stderr) => {
